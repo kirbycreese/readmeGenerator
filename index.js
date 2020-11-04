ae7fc1a30b5fe04a5386364edf00ed36d3cc3bc1
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-11-03 21:53:24 -0500

    latest version

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const inquirer = require ("inquirer");
 const fs = require ("fs");
-const generateMarkdown = require ("generateMarkdown")
+const generateMarkdown = require ('./generateMarkdown')
 // array of questions for user
 const questions = [
     {
         type: "input",
-        message: "Github username?",
+        message: "Enter Github username",
         name: "username"
-    }
+    },
+    {
+        type: "input",
+        message: "Enter email address",
+        name: "Email"
+    },
+    {
+        type: "input",
+        message: "Enter title of project",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Give a description of your project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What necessary dependencies must be installed to run this app?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "What is that app used for?",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        choices:[ "MIT", "ISC", "APACHE 2.0", "GPL 3.0", "BSD 3.0"],
+        message: "What license was used for this README?",
+        name: "License"
+    },
+    {
+        type: "input",
+        message: "Any contributors?",
+        name: "Contributor"
+    },
+    {
+        type: "input",
+        message: "What command is used to test this app?",
+        name: "Test"
+    },
 ];
 
 // function to write README file
@@ -25,7 +66,7 @@ function init() {
     inquirer.prompt(questions)
     .then(function(data){
         console.log(data)
-        writeToFile("README.md", generatorMarkdown(data));
+        writeToFile("README.md", generateMarkdown(data));
     })
 }
 
